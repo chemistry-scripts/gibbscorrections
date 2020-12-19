270ee56e1176fa196dff35ad01e4022add30033c
Emmanuel Nicolas <LaTruelle@users.noreply.github.com>
2020-12-19 15:16:40 +0100

    Starting structure for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gibbscorrections',
+      version='0.1',
+      description='',
+      url='http://github.com/chemistry-scripts/gibbscorrections',
+      author='Emmanuel Nicolas',
+      author_email='latruelle@users.noreply.github.com',
+      license='MIT',
+      packages=['gibbscorrections'],
+      zip_safe=False)
