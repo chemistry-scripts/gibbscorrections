d5e594b50d74f6787d53dd075dd6602a3de84a8e
Emmanuel Nicolas <LaTruelle@users.noreply.github.com>
2021-01-04 22:43:35 +0100

    PyPi upload 0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
-setup(name='gibbscorrections',
-      version='0.1',
-      description='',
-      url='http://github.com/chemistry-scripts/gibbscorrections',
-      author='Emmanuel Nicolas',
-      author_email='latruelle@users.noreply.github.com',
-      license='MIT',
-      packages=['gibbscorrections'],
-      zip_safe=False)
+long_description = (
+    "A module that automatises the tedious process of retrieving a Gaussian 16 geometry, and setting up and running"
+    + "a single point Orca calculation at that geometry. Of course, for a bunch of files at the same time."
+)
+
+setup(
+    name="gibbscorrections",
+    version="0.2",
+    description=long_description,
+    url="http://github.com/chemistry-scripts/gibbscorrections",
+    author="Emmanuel Nicolas",
+    author_email="latruelle@users.noreply.github.com",
+    license="MIT",
+    packages=["gibbscorrections"],
+    zip_safe=False,
+)
